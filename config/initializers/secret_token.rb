12cdc0d04e6058f30b50c8248955b75765ca7241
Alexey Pedyashev <alexey@idealernetwork.com>
2014-02-27 18:00:09 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4PennyAuction::Application.config.secret_key_base = 'f34b3e993de84f077509eb1a142f70f706b8d4618c195320ff5f2211d5fb136da851727a36c9886b45af3ff4b94a3e3b53ef6bfe9815798b0987d6571713974c'
